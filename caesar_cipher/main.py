460589ad4fd5b69d51a21e44ddf0976b7a657f8d
kimpowerrrrr <142423231+kimpowerrrrr@users.noreply.github.com>
2024-01-16 22:26:26 +0800

    Update main.py

--- caesar_cipher/main.py
+++ caesar_cipher/main.py
@@ -1,45 +1,48 @@
 import art
 
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+end_cipher = False
 
 def caesar(text, shift, direction):
-
     for letter in text:
-        position = alphabet.index(letter) + 1
-        if direction.lower() == "encode":
-            new_position = position + shift
-            print(f"starting position: {new_position} \nletter position: {position} \ntotal index of alphabet: {len(alphabet)}")
+        if letter in alphabet:
 
-            while new_position > len(alphabet):
-                new_position = new_position - len(alphabet)
-                print(f" test new position: {new_position}")
+            position = alphabet.index(letter) + 1
+            if direction.lower() == "encode":
+                new_position = position + shift
+                while new_position > len(alphabet):
+                    new_position = new_position - len(alphabet)
 
-            shifted_letters.append(alphabet[new_position - 1])
-            #else:
-            #    shifted_letters.append(alphabet[new_position])
-        elif direction.lower() == "decode":
-            new_position = position - shift
+                shifted_letters.append(alphabet[new_position - 1])
 
-            while new_position > len(alphabet):
+            elif direction.lower() == "decode":
+                new_position = position - shift
                 new_position = new_position + len(alphabet)
-                print(f" test new position: {new_position}")
+                while new_position > len(alphabet):
+                    new_position = new_position - len(alphabet)
+                    print(alphabet[new_position - 1])
 
-            shifted_letters.append(alphabet[new_position - 1])    
-            #if new_position > len(alphabet):
-            #    new_position = new_position + len(alphabet)
-            #    shifted_letters.append(alphabet[new_position])
-            #else:
-            #    shifted_letters.append(alphabet[new_position])
+                shifted_letters.append(alphabet[new_position - 1])    
+        else:
+            position = text.index(letter)
+            shifted_letters.append(text[position])
 
     message = ''.join(shifted_letters)
     print(f"{direction}d message: {message}")
 
 print(art.logo)
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-shifted_letters = []
+while end_cipher == False:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shifted_letters = []
 
-caesar(text, shift, direction)
+    shift = shift % 26
+    caesar(text, shift, direction)
+
+    continue_use = input("Do you want to continue? Y/N\n")
+    if continue_use.lower() == "n":
+        print("Goodbye!")
+        end_cipher = True
 
